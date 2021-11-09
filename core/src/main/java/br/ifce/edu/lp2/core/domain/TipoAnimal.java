ec7c1eaeb9d5194e22ff6ea4c94b198b5f5e8044
brenobccc <brenobritobc@gmail.com>
2021-11-09 11:13:40 -0300

    criando classes

---
+++ core/src/main/java/br/ifce/edu/lp2/core/domain/TipoAnimal.java
@@ -0,0 +1,11 @@
+package br.ifce.edu.lp2.core.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TipoAnimal {
+    private String id;
+    private String descricao;
+}
