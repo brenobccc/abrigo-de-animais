d6aa6e2033831524efdc02f27fdda205a6818730
brenobccc <brenobritobc@gmail.com>
2021-12-02 14:59:24 -0300

    bug coneção com mongodb

--- core/src/main/java/br/ifce/edu/lp2/core/us/CreateUserUS.java
+++ core/src/main/java/br/ifce/edu/lp2/core/us/CreateUserUS.java
@@ -1,18 +1,22 @@
 package br.ifce.edu.lp2.core.us;
 
 import br.ifce.edu.lp2.core.domain.Usuario;
+import br.ifce.edu.lp2.core.ports.driven.repository.SaveUserRepositoryPort;
 import br.ifce.edu.lp2.core.ports.driver.CreateUserPort;
-
-import java.util.UUID;
 
 //Cria uma história de usuário
 
-public class CreateUserUS implements CreateUserPort{
+public record CreateUserUS(SaveUserRepositoryPort saveUserRepositoryPort)
+        implements CreateUserPort{
 
     @Override
     public String apply(Usuario user){
-        System.out.println("Salvar no banco de dados");
-        return UUID.randomUUID().toString();
+
+        //VERIFICAR SE EXISTE ALGUM COM O ID IGUAL
+        //SALVAR NO BD
+            var id = saveUserRepositoryPort.apply(user);
+            System.out.println("Salvo no Banco de Dados");
+            return id;
     }
 
 }
